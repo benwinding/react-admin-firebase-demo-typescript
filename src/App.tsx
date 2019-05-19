1b5b45d025c9e7fe414fcfdc3e1c151f94397a3a
Ben Winding <ben.winding@gmail.com>
2019-05-19 16:09:42 +0930

    Working with types in pacakage

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,36 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
+import { PostList, PostShow, PostCreate, PostEdit } from "./posts";
+import { Admin, Resource } from "react-admin";
+import {
+  FirebaseRealTimeSaga,
+  FirebaseDataProvider,
+  FirebaseAuthProvider
+} from "react-admin-firebase";
 
-class App extends Component {
+const config = require("./FIREBASE_CONFIG.js").firebaseConfig;
+
+const dataProvider = FirebaseDataProvider(config);
+const options = {
+  observe: ["posts"]
+};
+const firebaseRealtime = FirebaseRealTimeSaga(dataProvider, options);
+
+class App extends React.Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Admin
+        customSagas={[firebaseRealtime]}
+        dataProvider={dataProvider}
+        authProvider={FirebaseAuthProvider}
+      >
+        <Resource
+          name="posts"
+          list={PostList}
+          show={PostShow}
+          create={PostCreate}
+          edit={PostEdit}
+        />
+      </Admin>
     );
   }
 }
---
+++ src/posts.tsx
@@ -0,0 +1,77 @@
+// in src/posts.js
+import * as React from "react";
+// tslint:disable-next-line:no-var-requires
+import {
+  Datagrid,
+  List,
+  Show,
+  Create,
+  Edit,
+  Filter,
+  DisabledInput,
+  SimpleShowLayout,
+  SimpleForm,
+  TextField,
+  TextInput,
+  ShowButton,
+  EditButton,
+  DeleteButton,
+  RichTextField,
+  SelectInput
+} from "react-admin";
+import RichTextInput from "ra-input-rich-text";
+
+const PostFilter = (props: any) => {
+  return (<Filter {...props}>
+    <TextInput label="Search" source="title" alwaysOn />
+  </Filter>);
+};
+
+export const PostList = (props: any) => (
+  <List {...props} filters={<PostFilter />}>
+    <Datagrid>
+      <TextField source="title" />
+      <RichTextField source="body" />
+      <ShowButton label="" />
+      <EditButton label="" />
+      <DeleteButton label="" />
+    </Datagrid>
+  </List>
+);
+
+export const PostShow = (props: any) => (
+  <Show {...props}>
+    <SimpleShowLayout>
+      <TextField source="id" />
+      <TextField source="title" />
+      <RichTextField source="body" />
+    </SimpleShowLayout>
+  </Show>
+);
+
+export const PostCreate = (props: any) => (
+  <Create {...props}>
+    <SimpleForm>
+      <TextInput source="title" />
+      <RichTextInput source="body" />
+    </SimpleForm>
+  </Create>
+);
+
+export const PostEdit = (props: any) => (
+  <Edit {...props}>
+    <SimpleForm>
+      <DisabledInput source="id" />
+      <TextInput source="title" />
+      <RichTextInput source="body" />
+      <SelectInput
+        source="rating"
+        choices={[
+          { id: 1, name: "Good" },
+          { id: 2, name: "Okay" },
+          { id: 3, name: "Bad" }
+        ]}
+      />
+    </SimpleForm>
+  </Edit>
+);
